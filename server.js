4690c8450de199a2362b56c7a771ebbe059661b0
Saja-alawneh <35113490+Saja-alawneh@users.noreply.github.com>
2021-02-11 11:21:10 -0500

    Add files via upload

--- server.js
+++ server.js
@@ -17,6 +17,25 @@ const routes = require('./lib/routes.js');
 // Load server configuration data
 const serverConfig = require('./configs/server.json');
 
+var fs = require('fs')
+const logFile = "./project.log";
+if (fs.existsSync(logFile)) {
+    fs.unlinkSync(logFile);
+}
+const log = require('simple-node-logger').createSimpleLogger('project.log');
+console.log = log.info;
+
+/*var pinoms = require('pino-multi-stream')
+var streams = [
+    {stream: fs.createWriteStream('/tmp/info.stream.out')},
+    {level: 'info', stream: fs.createWriteStream('/tmp/info.stream.out')}
+    ]
+var log = pinoms({streams: streams})
+
+log.info('this will be written to /tmp/info.stream.out')
+*/
+//log.fatal('this will be written to /tmp/fatal.stream.out')
+
 // Create a Hapi server instance
 // If you plan to deploy your hapi application to a PaaS provider, 
 // you must listen on host 0.0.0.0 rather than localhost or 127.0.0.1
@@ -28,7 +47,33 @@ const server = new Hapi.Server({
         stripTrailingSlash: true // removes trailing slashes on incoming paths
     }
 });
+//server.log(['test', 'error'], 'Test event');
+//server.events.on('log', (event, tags) => {
 
+  //  if (tags.error) {
+   //     console.log(`Server error: ${event.error ? event.error.message : 'unknown'}`);
+  //  }
+//});
+// Serevr Start Event
+server.events.on('start', () => {
+
+    //console.log('Server started');
+    console.log(server.info.started);       
+});
+server.events.on('log', (event) => {
+
+    console.log(`Server Event: ${event.timestamp ,event.data}`);
+    
+});
+// Request Event
+server.events.on('response', function (request) {
+    console.log(request.info.remoteAddress + ': ' + request.method.toUpperCase() + ' ' + request.path + ' --> ' + request.response.statusCode);
+});
+
+server.events.on('response', (request) => {
+
+    console.log(`Response sent for request: ${request.info.id}`);
+});
 //Server Stop Event
 server.events.on('stop', () => {
 
@@ -75,6 +120,8 @@ const init = async function() {
         layoutPath: './client/templates/layout',
         helpersPath: './client/templates/helpers'
     });
+
+
     function resSerializer(res) {
         return {
             body:res.raw.body
@@ -83,17 +130,23 @@ const init = async function() {
 
       await server.register({
         plugin: HapiPino,
-        options:{
-	    logRequestStart: false,
-	    logRequestComplete: false,
+        options:
+        {
             logPayload: true,
-	    redact: ['req.headers'],
-            serializers:{
+            prettyPrint:process.env.NODE_ENV !== 'production',
+            redact: ['req.headers.authorization'],
+            serializers:
+            {
             res: resSerializer
-        },
+            },
         }
     })
+server.logger.info('another way')
+const child=server.logger.child({a:'property'})
 
+server.log(['subsystem'], 'third way ')
+
+child.info('foo')
     // Start the server
     await server.start();
     console.log(`DeepPhe-Viz HTTP Server is running at: ${server.info.uri}`);
@@ -102,7 +155,7 @@ const init = async function() {
 };
 
 process.on('unhandledRejection', (err) => {
-    console.loggr(err);
+    console.log(err);
     process.exit(1);
 });
 
